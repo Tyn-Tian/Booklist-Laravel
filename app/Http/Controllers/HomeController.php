3aef84690c46afe087cf3977a4dc24fb7bc2a123
Christian <kristian2015ok@gmail.com>
2024-05-25 21:02:58 +0700

    Feat: Home Controller

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function home(Request $request): RedirectResponse
+    {
+        if ($request->session()->exists('user')) {
+            return redirect('/booklist');
+        } else {
+            return redirect('/login');
+        }
+    }
+}
