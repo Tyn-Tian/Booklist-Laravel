831fd44e7f18b674a11e65386c45a818d0891542
Christian <kristian2015ok@gmail.com>
2024-05-25 13:52:12 +0700

    Feat: user controller login

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,11 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class UserController extends Controller
 {
-    public function login()
+    public function login(): Response
     {
+        return response()
+            ->view('user.login', [
+                "title" => "Login"
+            ]);
     }
 
     public function doLogin()
